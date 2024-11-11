023ba0576096c562dc9dc7b7eea5893e98ff8558
WissamDouskary <wissamdouskary99@gmail.com>
2024-11-11 18:54:18 +0100

    some upgrades

--- js/about.js
+++ js/about.js
@@ -1,9 +1,1 @@
 // start ur code here
-let scroller = document.getElementById("scroller");
-let hieght = document.documentElement.scrollHeight - document.documentElement.clientHeight
-
-window.addEventListener("scroll", () => {
-    let scrollTop = document.documentElement.scrollTop
-    
-    scroller.style.width = `${(scrollTop / hieght) * 100}%`
-})
