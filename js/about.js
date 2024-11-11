6021ecac1d022e59d1295f4c475d6face60a9ca8
WissamDouskary <wissamdouskary99@gmail.com>
2024-11-11 22:40:58 +0100

    Creat a modal for Devis || linked it with Cart using JS

--- js/about.js
+++ js/about.js
@@ -1,1 +1,12 @@
 // start ur code here
+let modal = document.getElementById("modal");
+let checkoutBtn = document.getElementById("Checkout-btn");
+let modalCloseBtn = document.getElementById("modalCloseBtn");
+
+checkoutBtn.addEventListener("click",function(){
+    modal.style.display = "flex"
+    cartmenu.classList.add('translate-x-full');
+})
+modalCloseBtn.addEventListener("click" , function(){
+    modal.style.display = "none"
+})
