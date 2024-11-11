b627bcca2a17bf7c796796700668af2ab2d738f0
hamzagbouri <hamzagbouri2004@gmail.com>
2024-11-11 15:44:14 +0100

    working on panier

--- js/customize.js
+++ js/customize.js
@@ -1,9 +1,9 @@
 const id = localStorage.getItem('id')
 const productDetail = document.getElementById('product-detail')
 console.log(id)
+let firstTime = true;
 
 let connection = new XMLHttpRequest();
-let cart = []
 connection.open("GET", 'https://www.brofortech.com/data.json', true);
 connection.send();
 
@@ -51,12 +51,24 @@ connection.onreadystatechange = function () {
                                     <img src="../img/customize/Ellipse 65.png" alt="">
                                 </div>
                             </div>
-                            <p  class="cursor-pointer hover:underline">Add to Cart</p>
+                            <div class="flex items-center gap-4">
+                                    <label for="quantity" class="text-[#151875]">Quantity:</label>
+                                    <input 
+                                        type="number" 
+                                        id="quantity" 
+                                        min="1" 
+                                       value="1"
+                                        class="border border-gray-300 rounded w-16 text-center" 
+                                    />
+                                    <a id="addToCartt" class="cursor-pointer hover:underline text-[#1A0B5B]">Add to Cart</a>
+                                </div>
                             <p>Categories : Laptops</p>
                             <p>Tags : Pc , Good Quality</p>
                         </div>
                     </div>
             `
+            document.getElementById('addToCartt').addEventListener("click",()=> addToCart(pro))
+
         } else {
             console.error("Error fetching data: " + connection.status);
         }
@@ -64,8 +76,84 @@ connection.onreadystatechange = function () {
         
     }
 }
+function showOnCart(cart){
+    document.getElementById('item-list').innerHTML = ``
+   console.log("aa", cart)
+    cart.forEach(item => {
+        
+        const li = document.createElement('li')
+        li.className = 'flex justify-between text-sm text-gray-600'
+        li.innerHTML = `
+                        <div class="flex flex-col">
+                         <span>${item.name} (${item.Bquantity})</span>
+                         <div class="flex gap-4">
+                          <label for="cart-quantity" >Quantity:</label>
+                                    <input 
+                                        type="number" 
+                                        id="cart-quantity" 
+                                        min="1" 
+                                       value="${item.Bquantity}"
+                                       onchange="updateQuantity(${item.Bquantity}${item.id})"
+                                        class="border border-gray-300 rounded w-16 text-center" 
+                                    /> </div>
+                         </div>
+                        <span>${item.price * item.Bquantity} </span>`
+        document.getElementById('item-list').appendChild(li)
+    })
+}
+function updateQuantity(q,i){
+    const carrt = JSON.parse(localStorage.getItem('cart') )|| [];
+    for(let i =0; i< carrt.length;i++){
+        if (carrt[i].id == i){
+            carrt[i].Bquantity=parseInt(document.getElementById("cart-quantity").value)
+            break;
+        }
+   }
+   showOnCart(carrt)
+
+    localStorage.setItem("cart", JSON.stringify(carrt))
+
+}
+function addToCart(pro){
+    console.log(pro)
+    const carrt = JSON.parse(localStorage.getItem('cart') )|| [];
+   let quantity = parseInt(document.getElementById("quantity").value);
+
+  
+   
+    const it = {
+        id: pro.id,
+        name: pro.marque,
+        price: pro.price,
+        Bquantity: quantity
+    }
+    let found = false
+    let j
+   for(let i =0; i< carrt.length;i++){
+        if (carrt[i].id == it.id){
+            found = true;
+            j=i
+        }
+   }
+   if(found){
+    carrt[j].Bquantity +=  parseInt(document.getElementById("quantity").value)
+   } else{
+    carrt.push(it)
+   }
+   
+    showOnCart(carrt)
+
+    localStorage.setItem("cart", JSON.stringify(carrt))
+
+    
+        cartmenu.style.transform = "translateX(0)"
+    
+        document.getElementById('cart-close').addEventListener("click", function(){
+              cartmenu.style.transform = "translateX(100%)"
+        })
 
 
+}
 
 
 
