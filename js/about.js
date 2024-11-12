7a099e551dfa5aed64dfd6c6a1cc96a89aa7616c
WissamDouskary <wissamdouskary99@gmail.com>
2024-11-12 11:35:28 +0100

    creat a valid button on Cart and Configure it JS

--- js/about.js
+++ js/about.js
@@ -2,6 +2,7 @@
 let modal = document.getElementById("modal");
 let checkoutBtn = document.getElementById("Checkout-btn");
 let modalCloseBtn = document.getElementById("modalCloseBtn");
+let validationButton = document.getElementById("validation-btn");
 
 checkoutBtn.addEventListener("click",function(){
     modal.style.display = "flex"
@@ -9,4 +10,15 @@ checkoutBtn.addEventListener("click",function(){
 })
 modalCloseBtn.addEventListener("click" , function(){
     modal.style.display = "none"
+})
+
+
+validationButton.addEventListener("click" , function(){
+
+    isConfirme = confirm("Valid your cart !");
+
+    if(isConfirme){
+        modal.style.display = "flex"
+        cartmenu.classList.add('translate-x-full');
+    }
 })
